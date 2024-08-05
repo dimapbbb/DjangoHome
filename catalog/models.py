ee92e5c7579ccf09b3e7e77d4ddd8ba002bcce1b
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-08-05 20:12:06 +0300

    Созданы модели продукты и категории

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
+
+
+class Category(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
