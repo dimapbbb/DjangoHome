e8feab19a6924fab3737dd5992f7e61259dc8e4c
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-08-13 16:02:41 +0300

    создан шаблон для показа товаров задание 1,2

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URl = 'media/'
+MEDIA_URl = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
