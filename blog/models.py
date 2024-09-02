634325b4b0f2c907826a546f99ba5b256ed0de39
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-02 20:28:59 +0300

    Задание 2 Добавлена модель Версия

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class UserPost(models.Model):
     slug = models.CharField(max_length=150, verbose_name="slug", blank=True, null=True)
     content = models.TextField(verbose_name="Содержимое")
     picture = models.ImageField(upload_to="blog_picture/", verbose_name="Картинка", blank=True, null=True)
-    creation_date = models.DateTimeField(verbose_name="Дата создания", default=datetime.datetime.now())
+    creation_date = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
     publication_sign = models.BooleanField(default=False, verbose_name="Знак публикации")
     views_count = models.IntegerField(default=0, verbose_name="Просмотры")
 
